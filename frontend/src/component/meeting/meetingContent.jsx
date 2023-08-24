03735164adffc7a2bbd57405efb17f26f6a9345f
5tr1ker <tjseocld@naver.com>
2023-08-25 02:49:44 +0900

    feat: [Front] 모임 목록 이미지 사진 크키 설정
    feat: [Front] 모임 목록 이미지 사진 크키 설정

--- frontend/src/component/meeting/meetingContent.jsx
+++ frontend/src/component/meeting/meetingContent.jsx
@@ -1,10 +1,11 @@
 import '../../_style/meeting/meetingContent.css'
+import testImage from '../../_image/testImage.jpg'
 
 const MeetingContent = () => {
 
     return (
       <div className="overlap-group-2">
-          <img className="mask-group" alt="Mask group" src="image.png" />
+          <img className="mask-group" alt="Mask group" src={testImage} />
           <div className="rectangle-2" />
           <div className="text-wrapper-3">간단한 설명 정도</div>
           <div className="text-wrapper-4">경기도 김포시 사우동</div>
