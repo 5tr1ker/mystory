4c371f9c95854ef308b1d4f59b33d135533c098e
5tr1ker <tjseocld@naver.com>
2024-01-05 01:40:58 +0900

    fix: [Front] 방문자 기록이 출력되지 않는 버그 수정
    fix: [Front] 방문자 기록이 출력되지 않는 버그 수정

--- frontend/src/component/admin/content/main.jsx
+++ frontend/src/component/admin/content/main.jsx
@@ -4,7 +4,7 @@ import Chart from 'chart.js';
 import axios from "axios";
 
 const MainChartList = ({chart}) => {
-    if(chart === undefined || !Array.isArray(chart.map)) {
+    if(chart === undefined || Array.isArray(chart.map)) {
         return null;
     }
 
