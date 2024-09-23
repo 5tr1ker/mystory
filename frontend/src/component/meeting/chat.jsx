2d19ecac6bb9976d54ebd3ac112730a6753846cb
Sangjin Park <tjseocld@naver.com>
2024-09-23 11:54:30 +0900

    fix: socket.io 연결 주소 변경
    fix: socket.io 연결 주소 변경

--- frontend/src/component/meeting/chat.jsx
+++ frontend/src/component/meeting/chat.jsx
@@ -27,7 +27,7 @@ const Chat = ({ dropDownSet }) => {
   }, [chatData]);
 
   // Chatting 관련 끝
-  const webSocketUrl = `wss://server-springBoot:8080/chat`;
+  const webSocketUrl = `wss://mystorynews.net/chat`;
   let ws = useRef(null);
 
   const [socketConnected, setSocketConnected] = useState(false);
